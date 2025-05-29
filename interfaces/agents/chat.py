87e240b9d363666d343c0c4348ddc978dafe0a8f
Vaishakh Kallattil <vkallattil@gmail.com>
2025-05-29 02:17:18 -0400

    [adjustment] system prompting authority to system

--- interfaces/agents/chat.py
+++ interfaces/agents/chat.py
@@ -14,7 +14,7 @@ class ChatAgent(Agent[str, str]):
         self.system_prompt = system_prompt
         self.messages = [
           {
-            "role": "developer",
+            "role": "system",
             "content": system_prompt
           }
         ]
@@ -22,7 +22,7 @@ class ChatAgent(Agent[str, str]):
     def clear_messages(self):
         self.messages = [
           {
-            "role": "developer",
+            "role": "system",
             "content": self.system_prompt
           }
         ]
